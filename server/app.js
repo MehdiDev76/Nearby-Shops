ee3e74fdf399c9ced7df35c21ae1de1595bff336
MehdiDev76 <elkoukhelmehdi1@gmail.com>
2018-01-04 01:26:27 +0000

    setup of the express js server

---
+++ server/app.js
@@ -0,0 +1,35 @@
+var express          = require('express')
+    mongoose         = require('mongoose')
+    path             = require('path')
+    bodyParser       = require('body-parser')
+    cors             = require('cors')
+    expressValidator = require('express-validator')
+    session          = require('express-session')
+    passport         = require('passport')
+    
+    
+    
+
+var app = express()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(cors())
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log('Server has started!!!'+ 3000);
+  })
+  //to know the port
